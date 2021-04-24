a1ecad666ddb0abab42c2a7969b3215d830f01e0
Abulala Shaikh <64020957+Abulala10@users.noreply.github.com>
2021-04-25 02:16:30 +0530

    Update server_side_buzzer.py

--- server_side_buzzer.py
+++ server_side_buzzer.py
@@ -8,14 +8,14 @@ print('>>> Welcome Abulala Shaikh.\n>>> Simple program to turn the lights on and
 try:
 	gpio.setwarnings(False)
 	gpio.setmode(gpio.BCM)  
-	LED_PIN = 27
+	LED_PIN = 27  # Pin Number 27 on Raspberry pi.
 	gpio.setup(LED_PIN, gpio.OUT)
 except Exception as e:
 	print('General purpose input output error : ', e)
 
 
 try:
-	sock = socket.socket()
+	sock = socket.socket()  # Tcp/Ip connection.
 	host = ''
 	port = 8080
 except Exception as e:
@@ -26,12 +26,12 @@ def connection(host, port):
 	print('Waiting For Connection.........\n')
 	try:
 		sock.bind((host, port))
-		sock.listen(5)
+		sock.listen(5)  # listens to 5 connections
 		conn, addr = sock.accept()
 		print('Got Connection from : ', addr)
 		conn.send('Turn on the lights for me :)')
 		while True:
-			if conn.recv(1024) in ('on', 'ON', 'On'):
+			if conn.recv(1024) in ('on', 'ON', 'On'):  # receiving signals from Client.
 				gpio.output(LED_PIN, gpio.HIGH)
 				print('>>> Lights are ON')
 			elif conn.recv(1024) in ('of', 'OF', 'off', 'OFF', 'Of', 'Off'):
